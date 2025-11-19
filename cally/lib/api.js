992c0942865b4a83071fc3aa63d2f8c8c362a0ff
abhiiiiiii-21 <abhishekpatel12131@gmail.com>
2025-11-19 17:39:03 +0530

    added prod url

--- cally/lib/api.js
+++ cally/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ||
-  "http://localhost:3001";
+  "https://cally-k5rj.onrender.com";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
