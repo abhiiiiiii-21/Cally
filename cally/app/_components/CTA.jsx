10137c0bb69885b70b6a919e6a721bd45b9f0d04
abhiiiiiii-21 <abhishekpatel12131@gmail.com>
2026-01-09 01:31:04 +0530

    minor change

--- cally/app/_components/CTA.jsx
+++ cally/app/_components/CTA.jsx
@@ -6,7 +6,7 @@ import { Footer } from './Footer'
 const CTA = () => {
   return (
     <main>
-      <section className="relative w-6xl mx-auto overflow-hidden bg-black/20 text-black mb-30 mt-30 rounded-2xl font-urbanist flex justify-center items-center rounded-bl-2xl rounded-br-2xl">
+      <section className="relative w-6xl mx-auto overflow-hidden bg-black/20 text-black mb-30 mt-30 rounded-2xl font-urbanist flex justify-center items-center">
         <Image src="/Gradient/GreyCTA.jpg" alt="BG" layout="fill" objectFit="cover" className='opacity-40' />
         <ParallaxImage />
       </section>
